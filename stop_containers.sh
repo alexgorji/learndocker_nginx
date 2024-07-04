0bf26ac987f3d1db85dc6ac5014ddb54510bf0f7
alexgorji <aligorji@hotmail.com>
2024-07-04 17:22:37 +0200

    now with django site and variables

---
+++ stop_containers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker compose -f ./reverse_proxy/docker-compose.yml down
+docker compose -f ./django-site1/docker-compose.yml down
+docker compose -f ./site1/docker-compose.yml down
+docker compose -f ./site2/docker-compose.yml down
+
+docker network rm my-nginx-network
