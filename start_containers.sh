e516ed83980918b27a825a14823eae6d11ce4006
alexgorji <aligorji@hotmail.com>
2024-07-05 12:33:38 +0200

    simple vue 1 works

--- start_containers.sh
+++ start_containers.sh
@@ -4,4 +4,5 @@ docker network create my-nginx-network
 docker compose -f ./django-site1/docker-compose.yml up -d
 docker compose -f ./site1/docker-compose.yml up -d
 docker compose -f ./site2/docker-compose.yml up -d
+docker compose -f ./vue-site-1/docker-compose.yml up -d
 docker compose -f ./reverse_proxy/docker-compose.yml up -d
--- stop_containers.sh
+++ stop_containers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker compose -f ./reverse_proxy/docker-compose.yml down
+docker compose -f ./vue-site-1/docker-compose.yml down
 docker compose -f ./django-site1/docker-compose.yml down
 docker compose -f ./site1/docker-compose.yml down
 docker compose -f ./site2/docker-compose.yml down
