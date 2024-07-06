ae7ca560eb0929f985c585d839aa796798aab175
alexgorji <aligorji@hotmail.com>
2024-07-06 20:18:02 +0200

    demo projects refactored, prepared for makeing repositories out of them. NGINX_CONTAINER_NAME has been added to .env file for flexibility.

--- start_containers.sh
+++ start_containers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker network create my-nginx-network
-docker compose -f ./django-site1/docker-compose.yml up -d
-docker compose -f ./site1/docker-compose.yml up -d
-docker compose -f ./site2/docker-compose.yml up -d
-docker compose -f ./vue-site-1/docker-compose.yml up -d
+docker compose -f ./demo-simple-django-site/docker-compose.yml up -d
+docker compose -f ./demo-simple-html-site/docker-compose.yml up -d
+docker compose -f ./demo-simple-vue-site/docker-compose.yml up -d
 docker compose -f ./reverse_proxy/docker-compose.yml up -d
--- stop_containers.sh
+++ stop_containers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 docker compose -f ./reverse_proxy/docker-compose.yml down
-docker compose -f ./vue-site-1/docker-compose.yml down
-docker compose -f ./django-site1/docker-compose.yml down
-docker compose -f ./site1/docker-compose.yml down
-docker compose -f ./site2/docker-compose.yml down
+docker compose -f ./demo-simple-vue-site/docker-compose.yml down
+docker compose -f ./demo-simple-django-site/docker-compose.yml down
+docker compose -f ./demo-simple-html-site/docker-compose.yml down
 
 docker network rm my-nginx-network
